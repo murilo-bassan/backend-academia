fba4d7fcd5ce53ba6f5c248ac6288225a9a9e9e7
murilo_bassan <murilogabassan@gmail.com>
2025-09-26 23:32:06 -0400

    feat: findById de aluno

--- src/main/java/com/acad/sys/Repository/PagamentoRepositorio.java
+++ src/main/java/com/acad/sys/Repository/PagamentoRepositorio.java
@@ -1,8 +1,19 @@
 package com.acad.sys.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.acad.sys.Model.Aluno;
 import com.acad.sys.Model.Pagamento;
 
 public interface PagamentoRepositorio extends JpaRepository<Pagamento, Long> {
+
+    List<Pagamento> findByAluno(Aluno aluno);
+    
+    // Buscar os últimos 5 pagamentos, ordenados por data em ordem decrescente
+    @Query("SELECT p FROM Pagamento p ORDER BY p.data DESC")
+    List<Pagamento> findTop5ByOrderByDataDesc();
 
 }
