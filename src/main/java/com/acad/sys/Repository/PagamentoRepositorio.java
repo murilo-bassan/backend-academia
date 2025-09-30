d3edd20526a28f4cd25ab98e8042b9f858b8bcb4
murilo_bassan <murilogabassan@gmail.com>
2025-09-30 08:24:14 -0400

    feat: encontra o último pagamento do aluno (caso possua)

--- src/main/java/com/acad/sys/Repository/PagamentoRepositorio.java
+++ src/main/java/com/acad/sys/Repository/PagamentoRepositorio.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 import com.acad.sys.Model.Aluno;
 import com.acad.sys.Model.Pagamento;
 
@@ -13,7 +15,9 @@ public interface PagamentoRepositorio extends JpaRepository<Pagamento, Long> {
     List<Pagamento> findByAluno(Aluno aluno);
     
     // Buscar os últimos 5 pagamentos, ordenados por data em ordem decrescente
-@Query("SELECT p FROM Pagamento p ORDER BY p.data DESC LIMIT 5")
+    @Query("SELECT p FROM Pagamento p ORDER BY p.data DESC LIMIT 5")
     List<Pagamento> findTop5ByOrderByDataDesc();
+
+    Optional<Pagamento> findTopByAlunoIdOrderByVencimentoDesc(Long idAluno);
 
 }
